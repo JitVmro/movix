0b08e878642e9b7599b9d69b38f9e4bff9c7da67
Swapnajit Mishra <swapnajitmishra281@gmail.com>
2023-08-19 20:40:07 +0530

    Project Movix

---
+++ src/pages/404/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.scss'
+const PageNotFound = () => {
+  return (
+    <div>
+      PageNotFOund
+    </div>
+  )
+}
+
+export default PageNotFound
---
+++ src/pages/details/carousels/Similar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import Carousel from "../../../components/carousel/Carousel";
+import useFetch from "../../../hooks/useFetch";
+
+const Similar = ({ mediaType, id }) => {
+  const { data, loading, error } = useFetch(`/${mediaType}/${id}/similar`);
+
+  const title = mediaType === "tv" ? "Similar TV Shows" : "Similar Movies";
+
+  return (
+    <Carousel
+      title={title}
+      data={data?.results}
+      loading={loading}
+      endPoint={mediaType}
+    />
+  );
+};
+
+export default Similar;
+
+
+// import React from "react";
+
+// import Carousel from "../../../components/carousel/Carousel";
+// import useFetch from "../../../hooks/useFetch";
+
+// const Recommendation = ({ mediaType, id }) => {
+//     const { data, loading, error } = useFetch(
+//         `/${mediaType}/${id}/recommendations`
+//     );
+
+//     return (
+//         <Carousel
+//             title="Recommendations"
+//             data={data?.results}
+//             loading={loading}
+//             endpoint={mediaType}
+//         />
+//     );
+// };
+
+// export default Recommendation;
